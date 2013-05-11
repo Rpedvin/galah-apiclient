41f2568313968ab505c5f74a70d0fbdf201e2888
brownhead <jsull003@ucr.edu>
2013-05-11 11:22:28 -0700

    Relicensed under Apache License v2.

--- README.md
+++ README.md
@@ -11,13 +11,5 @@ links to downloads and documentation.
 
 ## License
 
-The Galah API Client is licensed separately from Galah. It is "licensed" under
-[The Unlicense](http://www.unlicense.org). All documentation for the API client
-has also been placed under The Unlicense.
-
-In short, feel free to use the code and documentation in this repository for any
-purpose without restriction (though keep in mind this definitely does not apply
-to the main Galah repository).
-
-Also, feel free to create your own version of the API Client! If you do, I'd
-gladly put a link up in this repo.
+The Galah API Client is licensed separately from Galah. It is licensed under
+the Apache License v2 (see the LICENSE file).
--- api_client.py
+++ api_client.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013 Galah Group LLC
+# Copyright (c) 2013 Other contributers as noted in the CONTRIBUTERS file
+#
+# This file is part of galah-apiclient.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 import json
 import os
@@ -549,7 +566,7 @@ def main(options, args):
             print >> sys.stderr, "\t" + str(e)
 
             exit(1)
-            
+
         try:
             api_info_file = open(api_info_file_path, "w")
         except IOError:
