4e89fa36e99152d5f24e39d431cd265015d48b46
brownhead <jsull003@ucr.edu>
2013-05-25 18:28:04 -0700

    Finished adding sphinx-like documentation.
    * Don't actually know if the documentation is properly formatted, and
      we won't know until we actually create documentation for this which
      may or may not happen.

--- apiclient/ui.py
+++ apiclient/ui.py
@@ -1,3 +1,26 @@
+# Copyright (c) 2013 Galah Group LLC
+# Copyright (c) 2013 Other contributers as noted in the CONTRIBUTERS file
+#
+# This file is part of galah-apiclient.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module handles most of the direct interaction with the user during normal
+operation.
+
+"""
+
 import sys
 import getpass
 import re
@@ -16,6 +39,11 @@ def parse_raw_args(args):
     :param args: The arguments the user provided (as provided by
             :data:`apiclient.config.ARGS`).
     :returns: A tuple ``(args, kwargs)``.
+
+    .. code-block:: text
+
+        >>> ui.parse_raw_args(["first", "second", "bar=apple sauce", "boom=bang"])
+        (['first', 'second'], {'bar': 'apple sauce', 'boom': 'bang'})
 
     """
 
@@ -58,6 +86,8 @@ def determine_credentials():
     This is done by checking the configuration and environmental variables for
     the needed values, and prompting the user when they are unavailable.
 
+    :returns: ``(user, password)``
+
     """
 
     if "user" in config.CONFIG:
@@ -81,13 +111,47 @@ def determine_credentials():
 
 import sys
 def print_carriage(text, width = 72):
+    """
+    Prints some text to standard out, followed by a carriage return. Also
+    flushes standard out afterwards.
+
+    """
+
     sys.stdout.write("\r" + str(text) + " " * (width - len(text)) + "\r")
     sys.stdout.flush()
 
 def progress_bar_indeterminate(size = 20, ball = "#####"):
+    """
+    A generator function that creates an indeterminate progress bar.
+
+    :param size: The width of the progress bar.
+    :param ball: The characters to use for the ball.
+
+    .. code-block:: python
+
+        >>> a = ui.progress_bar_indeterminate(7, "--")
+        >>> next(a)
+        '[ --  ]'
+        >>> next(a)
+        '[  -- ]'
+        >>> next(a)
+        '[   --]'
+        >>> next(a)
+        '[  -- ]'
+        >>> next(a)
+        '[ --  ]'
+        >>> next(a)
+        '[--   ]'
+
+    """
+
     position = 0
     going_right = True
-    adjusted_size = size - len(ball)
+    adjusted_size = size - len(ball) - 2
+
+    if adjusted_size <= 1:
+        raise ValueError("size not big enough.")
+
     while True:
         position += 1 if going_right else -1
 
@@ -101,6 +165,24 @@ def progress_bar_indeterminate(size = 20, ball = "#####"):
         yield "".join(result)
 
 def progress_bar(progress, size = 20):
+    """
+    Returns an ASCII progress bar.
+
+    :param progress: A floating point number between 0 and 1 representing the
+            progress that has been completed already.
+    :param size: The width of the bar.
+
+    .. code-block:: python
+
+        >>> ui.progress_bar(0.5, 10)
+        '[####    ]'
+
+    """
+
+    size -= 2
+    if size <= 0:
+        raise ValueError("size not big enough.")
+
     if progress < 0:
         return "[" + "?" * size + "]"
     else:
--- apiclient/utils.py
+++ apiclient/utils.py
@@ -1,3 +1,26 @@
+# Copyright (c) 2013 Galah Group LLC
+# Copyright (c) 2013 Other contributers as noted in the CONTRIBUTERS file
+#
+# This file is part of galah-apiclient.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module contains any useful function that don't fit in any of the other
+modules.
+
+"""
+
 def json_module():
     """
     A handy function that will try to find a suitable JSON module to import and
@@ -43,6 +66,11 @@ def yaml_module():
     return yaml
 
 def requests_module():
+    """
+    Imports the request library and returns it.
+
+    """
+
     try:
         import requests
     except ImportError:
@@ -50,13 +78,37 @@ def requests_module():
             "Could not load the requests library. Please install requests "
             "to correct this problem."
         )
+
     return requests
 
 import os.path
 def resolve_path(path):
+    """
+    Converts a path a user provided into its absolute path representation. This
+    will behave as the user expects.
+
+    :param path: A relative or absolute path to convert.
+    :returns: An absolute path pointing to `path`
+
+    """
+
     return os.path.abspath(os.path.expanduser(path))
 
 def resolve_paths(paths):
+    """
+    Resolves a list of paths.
+
+    Equivalent to
+
+    .. code-block:: python
+
+        [resolve_path(i) for i in paths]
+
+    :param paths: An iterable containing paths to convert.
+    :returns: A list containing the converted paths.
+
+    """
+
 	return [resolve_path(i) for i in paths]
 
 def to_json(obj):
@@ -64,7 +116,6 @@ def to_json(obj):
     Serializes an object into a JSON representation.
 
     :param obj: The object to serialize.
-
     :returns: A JSON object (encoded as a string) representing ``obj`` that is
             compressed apprropriately for network transfer.
 
@@ -73,21 +124,61 @@ def to_json(obj):
     return json_module().dumps(obj, separators = (",", ":"))
 
 def load_yaml(file):
+    """
+    Loads a YAML file safely.
+
+    :param file: A file object to load.
+    :returns: The deserialized contents of the file.
+
+    """
+
     return yaml_module().safe_load(file)
 
 import os.path
 import os
-def prepare_directory(path):
+def prepare_directory(path, permissions = 0o700):
+    """
+    Ensures a directory exists and creates it if necessary, including any
+    necessary parent directories.
+
+    :param path: The directory to create:
+    :param permissions: The permissions to give the directories. Ignored on
+            non nix systems.
+    :returns: `True` iff a new directory was created, `False` otherwise.
+
+    Permissions will not be changed if the directory was already created.
+
+    """
+
     directory = os.path.dirname(path)
 
     if os.path.isdir(directory):
         return False
 
-    os.makedirs(directory, 0o700)
+    os.makedirs(directory, permissions)
     return True
 
 HOME_DIR = os.path.expanduser("~")
 def shorten_path(path):
+    """
+    Attempts to return a "friendly" version of the given path.
+
+    :param path: The path to shorten.
+    :returns: A friendly version of `path`
+
+    .. code-block:: text
+
+        >>> utils.shorten_path("/tmp/walrus")
+        '/tmp/walrus'
+        >>> os.getcwd()
+        '/home/john/Projects/galah-apiclient/apiclient'
+        >>> utils.shorten_path("/home/john/Projects/galah-apiclient")
+        '~/Projects/galah-apiclient'
+        >>> utils.shorten_path("/home/john/Projects/galah-apiclient/apiclient")
+        '~/Projects/galah-apiclient/apiclient'
+
+    """
+
     choices = []
 
     choices.append(path)
@@ -99,6 +190,17 @@ def shorten_path(path):
     return min(choices, key = len)
 
 def postfix_file_name(file_name, suffix):
+    """
+    Adds a suffix to a filename while preserving the file extension.
+
+    :returns: The modified file_name.
+
+    .. code-block:: text
+
+        >>> utils.postfix_file_name("myfile.exe", " walrus")
+        'myfile walrus.exe'
+    """
+
     file_name, extension = os.path.splitext(file_name)
 
     return str(file_name) + str(suffix) + str(extension)
@@ -109,6 +211,15 @@ def find_available_file(file_path):
     taken. For example, if a file myfile.tgz exists, we will use
     myfile (1).tgz. This is similar to how the browser chromium handles
     downloads.
+
+    :param file_path: The most desired file path.
+    :returns: The first available file path, adding (#) to the end of the file
+            name as necessary.
+
+    .. code-block:: text
+
+        >>> utils.find_available_file("/tmp/my_monkey.exe")
+        '/tmp/my_monkey (3).exe'
 
     """
 
