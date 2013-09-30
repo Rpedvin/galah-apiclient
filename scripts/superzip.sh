c18c2806ac46da83a5e2db0c6a86006a3e7b95f5
brownhead <jsull003@ucr.edu>
2013-09-30 13:29:00 -0700

    Added safety to build script.

--- scripts/superzip.sh
+++ scripts/superzip.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# This will ensure that the script exits if a failure occurs
+set -e
+
+# This will ensure the user is visually prompted upon failure
+trap "echo FAILURE: An error has occured! >&2" EXIT
+
 # Get the directory the script is in.
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -7,3 +13,9 @@ echo "Building superzip of the Galah API client from local repo."
 
 set -x
 superzippy -vvv -o galapi "PyYAML --global-option='--without-libyaml'" "$DIR/.." apiclient.main:main
+
+echo "Done."
+
+# Unset the trap so we don't freak the user out by telling them an error has
+# occured when everything went fine.
+trap - EXIT
